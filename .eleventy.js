ceb71636e62cbce54754edb37cb6a8436c1103a0
Schuyler Erle <schuyler@nocat.net>
2026-03-25 12:47:57 -0700

    Add GitHub Pages deployment with custom domain atgeo.org
    GitHub Actions workflow builds with Eleventy and deploys to Pages.
    CNAME file sets the custom domain.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   eleventyConfig.addShortcode("mapDemo", function () {
     return '<div id="map-demo" class="widget"></div>';
